8dd04a60b3434ec1b267aace88ce8d6a2fca581e
4kbr <igyutubfesbuk@gmail.com>
2023-01-29 21:53:48 +0700

    client side rendering

--- pages/last-sales.tsx
+++ pages/last-sales.tsx
@@ -1,10 +1,51 @@
+import { useEffect, useState } from "react";
+
 export default function LastSalePage() {
-  // useEffect(() => {
-  //   fetch("/");
-  // }, []);
+  const [sales, setSales] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    console.log("test ini");
+    fetch(`${process.env.NEXT_URL}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log({ data });
+        const transformSales = [];
+        for (const key in data) {
+          transformSales.push({
+            id: key,
+            username: data[key].username,
+            favorit: data[key].favorit,
+          });
+          // setSales((prev) => [
+          //   ...prev,
+          //   {
+          //     id: key,
+          //     username: data[key].username,
+          //     favorit: data[key].favorit,
+          //   },
+          // ]);
+        }
+        setSales(transformSales);
+        setIsLoading(false);
+      });
+  }, []);
   console.log("ini last sales");
 
-  return <div>LastSalePage</div>;
+  if (isLoading) return <p>loading...</p>;
+
+  if (!sales) return <p>No data here</p>;
+
+  return (
+    <ul>
+      {sales.map((sale: any) => (
+        <li key={sale.id}>
+          {sale.username} - food: {sale.favorit}
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 // export const
