d66442295ce25e9921f647a3833f2a7d88545e06
4kbr <igyutubfesbuk@gmail.com>
2023-01-29 17:55:46 +0700

    skip client side

---
+++ pages/last-sales.tsx
@@ -0,0 +1,10 @@
+export default function LastSalePage() {
+  // useEffect(() => {
+  //   fetch("/");
+  // }, []);
+  console.log("ini last sales");
+
+  return <div>LastSalePage</div>;
+}
+
+// export const
